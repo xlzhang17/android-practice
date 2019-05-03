dee73a48b9afb7c55cfdcc36f0ac8ad802e004e1
Candy zhang <xiuzhang@microsoft.com>
2019-05-03 21:44:45 +0800

    inite again

--- beatbox/src/main/java/com/example/beatbox/BeatBoxActivity.java
+++ beatbox/src/main/java/com/example/beatbox/BeatBoxActivity.java
@@ -1,13 +1,24 @@
 package com.example.beatbox;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BeatBoxActivity extends AppCompatActivity {
+public class BeatBoxActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.fragment_beat_box;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_beat_box);
+        setContentView(getLayoutResId());
     }
 }
---
+++ beatbox/src/main/java/com/example/beatbox/BeatBoxFragment.java
@@ -0,0 +1,12 @@
+package com.example.beatbox;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+
+    public static BeatBoxFragment newInstance() {
+
+        return new BeatBoxFragment();
+    }
+}
